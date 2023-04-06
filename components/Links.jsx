fdc0bad2c329837aed4014c69a162e4449d0577f
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-04-06 21:46:51 +0700

    feat: add links component
    act as navigation component

---
+++ components/Links.jsx
@@ -0,0 +1,20 @@
+function Links() {
+  const links = [
+    { title: 'about', url: '/' },
+    { title: 'projects', url: '/projects' },
+  ];
+
+  let activeLink = 'about';
+
+  return (
+    <div className="my-2 flex min-h-min flex-row gap-x-2">
+      {links.map((l) => (
+        <a href={l.url} key={l.title} className={`cursor-pointer border-[1px] border-black p-2 text-black transition-all duration-300 hover:bg-black hover:text-white ${activeLink === l.title ? '' : ''}`}>
+          <p>{l.title}</p>
+        </a>
+      ))}
+    </div>
+  );
+}
+
+export default Links;
