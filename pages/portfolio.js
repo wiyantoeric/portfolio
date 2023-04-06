02ab0cd4ee17c91be42c5f5d7c019ac5d9930f7d
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-04-06 21:48:06 +0700

    feat: create pages

---
+++ pages/portfolio.js
@@ -0,0 +1,11 @@
+import Container from '../components/Container';
+
+function portfolio() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
+
+export default portfolio;
---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import Container from '../components/Container';
+
+function projects() {
+  return (
+    <Container>
+      <div className="flex aspect-square w-[200px] flex-col border-[1px] border-black ">
+        
+      </div>
+    </Container>
+  );
+}
+
+export default projects;
