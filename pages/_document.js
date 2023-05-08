fff5c636e9d4751a6ec0694def3e19c079f3cab9
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2023-05-08 10:35:29 +0700

    feat: add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="description" content="Eric wiyanto portfolio website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
