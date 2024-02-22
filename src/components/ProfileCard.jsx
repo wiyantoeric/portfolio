cb29aeb6bd33eab9125ddcae45112017d940e500
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-02-22 19:46:58 +0700

    feat: add works button

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,12 +1,20 @@
+import Link from "next/link";
 import GithubIcon from "./icon/GithubIcon";
 import LinkedinIcon from "./icon/LinkedinIcon";
 
 function ProfileCard() {
   return (
-    <div className="flex aspect-[7/8] w-96 flex-col border-2 border-black p-8">
-      <div className="flex flex-col justify-start ">
+    <div className="relative flex aspect-[7/8] w-96 flex-col border-2 border-black p-8">
+      <Link href={'/works'} className="group absolute right-0 top-0 flex origin-top-right cursor-pointer flex-row gap-2">
+        <div className="border-b-2 border-l-2 border-black group-hover:bg-black">
+          <p className="border border-white border-opacity-0 p-1 group-hover:border-opacity-100 group-hover:text-white">
+            my works
+          </p>
+        </div>
+      </Link>
+      <div className="flex flex-col justify-start">
         <p className="text-2xl">Eric Wiyanto</p>
-        <p className="my-2 text-sm italic">Application development</p>
+        <p className="text-sm italic">application developer</p>
         <p className="my-4 whitespace-pre-line">
           {
             "Hey there!\n I'm currently on my Informatics degree. I do web and mobile programming with NextJs and Flutter"
