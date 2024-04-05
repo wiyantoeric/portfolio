141bfa2b3a9946a278bb31a05f293f4e4b18d6f7
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-04-05 14:50:53 +0700

    feat: add vercel SpeedInsights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,19 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Eric Wiyanto',
-  description: 'Eric Wiyanto\'s portfolio website',
-}
+  title: "Eric Wiyanto",
+  description: "Eric Wiyanto's portfolio website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className}`}>{children}</body>
+      <SpeedInsights />
     </html>
-  )
+  );
 }
