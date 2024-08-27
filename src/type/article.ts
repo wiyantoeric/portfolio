2d23e1def8245c4a096ef837ee3a5aae80aa7487
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-08-27 18:30:12 +0700

    feat: update article item

--- src/type/article.ts
+++ src/type/article.ts
@@ -1,8 +1,10 @@
-interface Project {
+export interface Project {
   title: string;
   link: string;
   alt: string;
+  description?: string;
   imageUrl?: string;
+  tags?: string[];
 }
 
 export interface Article {
